42b5e6f03763eb6158d2960ca5127beddf59f617
june3 <junecjstk@naver.com>
2025-02-11 17:11:15 +0900

    v1.1)

--- main.py
+++ main.py
@@ -6,32 +6,23 @@ def is_prime(num) -> bool:
     """
     if num >= 2:
         for i in range(2, int(num ** 0.5) + 1):
+        i = 2
+        while i < (int(num ** 0.5) + 1):
+        #for i in range(2, int(num ** 0.5) + 1):
             if num % i == 0:
                 return False
                 #is_prime = False
                 #break
             #print(i, end=' ')
+            i = i + 1
     else:
         return False
     return True
+
 # main
 #help(abs)
 help(is_prime)
+#help(is_prime)
 n = int(input("Input number : "))
-is_prime = True
-if n >= 2:
-    #for i in range(2, n):
-    for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
-            is_prime = False  #count = count + 1
-            break
-        print(i, end=' ')
-else:
-    is_prime = False
 
-#if count == 0:
-if is_prime:
 if is_prime(n):  # function call
-    print(f"{n} is prime number")
-else:
-    print(f"{n} is NOT prime number!")
